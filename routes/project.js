ac32a9804b5493b12f8ae2cab19ae9fa720974d9
Haley Kong <hgkong@stanford.edu>
2014-02-19 23:47:53 +0000

    putting it together lab

--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,10 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
+  models.Project
+    .find({"_id": projectID})
+    .sort('-date')
+    .exec(afterQuery);
   // query for the specific project and
   // call the following callback
 
@@ -16,6 +20,20 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  var newProject = new models.Project ({
+    "title" : form_data.project_title,
+    "date" : form_data.date,
+    "summary" : form_data.summary,
+    "image" : form_data.image_url,
+  })
+  newProject.save(afterSaving);
+
+  function afterSaving(err) { // this is a callback
+    if(err) {console.log(err); res.send(500); }
+    res.send();
+    res.redirect('/');
+}
+
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
@@ -23,6 +41,15 @@ exports.addProject = function(req, res) {
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(deleteCallback)
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+
+  function deleteCallback(err) {
+    if(err) { console.log(err); res.send(500);}
+    res.send();
+  }
 }
